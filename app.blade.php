09305c51bf594bb79927db2f001a81bc62694fbf
Dani-design <52079387+Dani-design@users.noreply.github.com>
2019-06-22 17:30:59 +0300

    Add files via upload

--- app.blade.php
+++ app.blade.php
@@ -1,10 +1,36 @@
 <!DOCTYPE html>
 <html>
 <head>
+  <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
   <meta charset="utf-8">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
   <title>ErasmusOppurtunities</title>
+  <script src="{{ asset('js/app.js') }}" defer></script>
+  <link rel="stylesheet" href="/css/app.css">
+  <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
 </head>
 <body>
-  @yied ('content')
+  @include ('inc.navbar')
+  <div class="container">
+    @include ('inc.showcase')
+    <div class="jumbotron text-center" id= "show">
+      <div class="container" >
+        <h1> Welcome</h1>
+        <p class="lead"> hello there </p>
+      </div>
+    </div>
+  <div class="row">
+    <div class="col-md-8 col-lg-8">
+      @yield ('content')
+    </div>
+    <div class="col-md-4 col-lg-4">
+      @include ('inc.sidebar')
+
+    </div>
+  </div>
+</div>
 </body>
 </html>
