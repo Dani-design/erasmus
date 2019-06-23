e0d58b39be309af51a60bba71edbd7b86f0e609e
Dani-design <52079387+Dani-design@users.noreply.github.com>
2019-06-23 12:47:27 +0300

    Add files via upload

--- app.blade.php
+++ app.blade.php
@@ -1,37 +1,49 @@
 <!DOCTYPE html>
-<html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-  <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-  <meta charset="utf-8">
-  <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>ErasmusOppurtunities</title>
 
-  <title>ErasmusOppurtunities</title>
-  <script src="{{ asset('js/app.js') }}" defer></script>
-  <link rel="stylesheet" href="/css/app.css">
-  <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
-  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+ <link rel="stylesheet" href="/css/app.css">
+ <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+ <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-  @include ('inc.navbar')
-  <div class="container">
-    @include ('inc.showcase')
-    <div class="jumbotron text-center" id= "show">
+    <div id="app">
+      <div class="container">
+      <div class="jumbotron text-center">
       <div class="container" >
         <h1> Welcome</h1>
         <p class="lead"> hello there </p>
       </div>
     </div>
-  <div class="row">
-    <div class="col-md-8 col-lg-8">
-      @include('inc.message')
-      @yield ('content')
-    </div>
-    <div class="col-md-4 col-lg-4">
-      @include ('inc.sidebar')
-
-    </div>
   </div>
+      @include ('inc.navbar')
+      <div class="container">
+     @include ('inc.message')
+              @yield('content')
+  <div>
+              <script src="/vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
+ <script>
+ CKEDITOR.replace('article-ckeditor');
+ </script>
 </div>
+          </div>
+        </main>
+    </div>
 </body>
 </html>
--- navbar.blade.php
+++ navbar.blade.php
@@ -1,5 +1,6 @@
+<!-- <div class="container">
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-<div class="container"
+<div class="container">
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
@@ -13,8 +14,97 @@
     <li class="nav-item">
       <a class="nav-link" href="/projectposts/create"> Crate a project </a>
     </li>
+    <@guest
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+        </li>
+         @if (Route::has('register'))
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+            </li>
+        @endif
+
+    @else
+        <li class="nav-item dropdown">
+            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                {{ Auth::user()->name }} <span class="caret"></span>
+            </a>
+
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="{{ route('logout') }}"
+                   onclick="event.preventDefault();
+                                 document.getElementById('logout-form').submit();">
+                    {{ __('Logout') }}
+                </a>
+
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                </form>
+            </div>
+        </li>
+    @endguest
     </ul>
 
   </div>
 </div>
 </nav>
+</div> -->
+<div class="container" id = "navbar">
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark" >
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+            <!--Left Side Of Navbar -->
+             <ul class="navbar-nav mr-auto">
+               <li class="nav-item active">
+                 <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+               </li>
+               <li class="nav-item">
+                 <a class="nav-link" href="/projectposts">Projects</a>
+               </li>
+               <a class="nav-link" href="/partners">Partners</a>
+               </li>
+               <li class="nav-item">
+               <a class="nav-link" href="/projectposts/create"> Crate a project </a>
+               </li>
+            </ul>
+
+            <!-- Right Side Of Navbar -->
+            <ul class="navbar-nav ml-auto">
+                <!-- Authentication Links -->
+                <@guest
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                    </li>
+                     @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                    @endif
+                @else
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
+
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                               onclick="event.preventDefault();
+                                             document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+                @endguest
+            </ul>
+        </div>
+    </div>
+</nav>
+</div>
